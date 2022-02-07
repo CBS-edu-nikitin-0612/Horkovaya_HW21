8d72e0665d6912403bfd307a2367df614bfc8794
Alina <alina.blank@gmail.com>
2022-02-07 16:28:21 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Task1
+{
+    delegate double MyDelegate(int x, int y, int z);
+    class Program
+    {
+        static MyDelegate Avg = delegate (int operato1, int operato2, int operato3)
+        {
+            return (double)(operato1 + operato2 + operato3) / 3;
+        };
+        static void Main(string[] args)
+        {
+            double res = Avg(5, 12, 23);
+            Console.WriteLine(res);
+        }
+    }
+}
